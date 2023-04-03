84cc47d981ea9aa469bd45851231e72becff8965
MahdiElradi <you@example.com>
2023-04-03 23:27:10 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/mahdielradi/untitled/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mahdielradi.untitled
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+// import 'package:tut_app/presentation/resources/theme_manager.dart';
+//
+// import '../presentation/products.dart';
+//
+// class MyApp extends StatefulWidget {
+//   MyApp._internal(); // named constructor
+//
+//   static final MyApp _instance =
+//       MyApp._internal(); // singleton or single instance
+//
+//   factory MyApp() => _instance; // factory
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       // initialRoute: Routes.splashRoute,
+//       home: const ProductsView(),
+//       // onGenerateRoute: RouteGenerator.getRoute,
+//       theme: getApplicationTheme(),
+//     );
+//   }
+// }
